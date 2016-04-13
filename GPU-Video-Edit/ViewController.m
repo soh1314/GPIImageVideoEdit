82b6a6b0405274476879be98fe9c878278a8238e
xiaoke_mh <xiaoke_mh@xiaoke-mac-pro-mdemacbook-pro.local>
2016-04-13 10:30:02 +0800

    Initial Commit

---
+++ GPU-Video-Edit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GPU-Video-Edit
+//
+//  Created by xiaoke_mh on 16/4/13.
+//  Copyright © 2016年 m-h. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
